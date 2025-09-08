0a9dd8abe6b11fa7ca342bce766f88cc65396921
ThePiemanYT <simchanvibol46@gmail.com>
2025-09-08 23:50:38 +0700

    Delete scripts directory

--- scripts/reload.js
+++
@@ -1,9 +0,0 @@
-export function reload(player, updateAmmoDisplay) {
-  if (player.ammo === player.magazineSize || player.reserveAmmo === 0) return;
-  const needed = player.magazineSize - player.ammo;
-  const toLoad = Math.min(needed, player.reserveAmmo);
-  player.ammo += toLoad;
-  player.reserveAmmo -= toLoad;
-  updateAmmoDisplay();
-  // Optionally play reload sound here
-}
